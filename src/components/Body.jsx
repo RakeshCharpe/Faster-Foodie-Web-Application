9b1e507631ebad032fcd37c99ecaaa4e731abc3a
RakeshCharpe <rakeshcharpe99@gmail.com>
2024-05-01 02:03:37 +0530

    updated the code

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,29 @@
-import restlists from "../Utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+import restlists from "../Utils/mockData";
 
 const Body = () => {
+  // React useState Hooks is used to change the state of components
+  const [filteredlist, setfilteredlist] = useState(restlists);
+  console.log(filteredlist);
   return (
     <div className="body">
-      <div className="search-Bar">Search ...</div>
+      <div className="filter-btn">
+        <button
+          className="btn"
+          onClick={() => {
+            const filtered = filteredlist.filter(
+              (res) => res.info.avgRating > 4
+            );
+            console.log(filtered);
+            setfilteredlist(filtered);
+          }}
+        >
+          Top Restaurants
+        </button>
+      </div>
       <div className="rest-container">
-        {restlists.map((restaurant) => (
+        {filteredlist.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
